a4822049fcba952c222563cbf863814322cf3726
aaron <aaron@classting.com>
2022-06-23 17:06:41 +0900

    chore: tailwind css 추가 및 설정
    Singed-off-by: kimts2627 <kimtstststs@gmail.com>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }} className="text-red-500">
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
