e0e5a0eec4f3870b5eea21a1527057d21c699431
aaron <aaron@classting.com>
2022-07-01 18:57:24 +0900

    feat: Navbar 컴포넌트를 정의하여 root page의 navigation 공간 확보
    Singed-off-by: kimts2627 <kimtstststs@gmail.com>

---
+++ app/components/common/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import type { ReactElement } from 'react';
+
+const Navbar = (): ReactElement => {
+  return (
+    <nav className="h-24 w-full flex justify-between items-center">
+      <span>로고</span>
+      <ul className="flex gap-8 ">
+        <li>dd</li>
+        <li>dd</li>
+        <li>dd</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,14 @@
 import type { ReactElement } from 'react';
 
 import RootTemplate from '~/templates/RootTemplate';
+import Navbar from '~/components/common/Navbar';
 
 const Index = (): ReactElement => {
   return (
     <RootTemplate.Root>
-      <RootTemplate.Header>헤더</RootTemplate.Header>
+      <RootTemplate.Header>
+        <Navbar />
+      </RootTemplate.Header>
       <RootTemplate.Content>컨텐츠</RootTemplate.Content>
     </RootTemplate.Root>
   );
