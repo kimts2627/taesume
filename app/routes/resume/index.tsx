846f4dfac2dde2237220e614ccbdc4e0b21f3119
aaron <aaron@classting.com>
2022-07-09 21:32:39 +0900

    feat: `/resume` 페이지에 ColoredOutline 적용
    - 필요하지 않다고 판단되는 scrollable props 제거
    
    Singed-off-by: kimts2627 <kimtstststs@gmail.com>

--- app/routes/resume/index.tsx
+++ app/routes/resume/index.tsx
@@ -2,15 +2,19 @@ import type { ReactElement } from 'react';
 
 import ResumeTemplate from '~/templates/ResumeTemplate';
 import Navbar from '~/components/common/Navbar';
+import ColoredOutline from '~/components/common/ColoredOutline';
 
 const ResumePage = (): ReactElement => {
   return (
-    <ResumeTemplate.Root>
-      <ResumeTemplate.Header>
-        <Navbar />
-      </ResumeTemplate.Header>
-      <ResumeTemplate.Content>gg</ResumeTemplate.Content>
-    </ResumeTemplate.Root>
+    <>
+      <ResumeTemplate.Root>
+        <ResumeTemplate.Header>
+          <Navbar />
+        </ResumeTemplate.Header>
+        <ResumeTemplate.Content>gg</ResumeTemplate.Content>
+      </ResumeTemplate.Root>
+      <ColoredOutline />
+    </>
   );
 };
 
